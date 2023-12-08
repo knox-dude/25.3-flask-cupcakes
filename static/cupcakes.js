4187f3134be6e77b9da56b688f33350bdda70f53
Andrew Knox <andrew.knox.r@gmail.com>
2023-12-08 15:29:44 -0800

    added completed project

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,14 +1,49 @@
-// need to generate html for the cupcakes.
 // also need to handle the form.
 
-addEventListener("DOMContentLoaded", evt => {
-  const cupcakeList = document.querySelector(".cupcake-list");
-  cupcakes = getCupcakesList();
-  console.log()
+function generateCupcakeHTML(cupcake) {
+  return `
+  <div>
+    <img class="cupcake-img" src="${cupcake.image}" alt="cupcake image" width="150px" height="auto">
+    <li data-id=${cupcake.id}>
+      Flavor: ${cupcake.flavor} <br>
+      Rating: ${cupcake.rating} <br>
+      Size: ${cupcake.size} <br>
+    </li>
+  </div>
+  `;
+}
+
+async function getCupcakeData() {
+  const data = (await axios.get("/api/cupcakes")).data;
+  return data
+}
+
+async function showCupcakes() {
+  let data = await getCupcakeData();
+  for (let cupcake of data.cupcakes) {
+    let html = $(generateCupcakeHTML(cupcake));
+    $(".cupcake-list").append(html);
+  }
+}
+
+$("form").on("submit", async function (evt) {
+  evt.preventDefault();
+  let flavor = $("#flavor").val();
+  let rating = $("#rating").val();
+  let size = $("#size").val();
+  let image = $("#image").val();
+
+  const resp = await axios.post("/api/cupcakes", {
+    flavor,
+    rating,
+    size,
+    image
+  });
+
+  let html = $(generateCupcakeHTML(resp.data.cupcake))
+  $(".cupcake-list").append(html);
+  $("form").trigger("reset");
 })
 
-async function getCupcakesList() {
-  const cupcakes = await axios.get("/api/cupcakes")
-  console.log(cupcakes)
-  return cupcakes
-}
+addEventListener("DOMContentLoaded", showCupcakes);
+
